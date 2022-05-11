32b6709ea115c87b4c0b9b9a8b75dada0555a010
grouxmathiasMGX <58417755+grouxmathiasMGX@users.noreply.github.com>
2022-05-11 16:52:02 +0200

    Affichage + modification d'un utilisateur
    Il faut terminer la modification de l'utilisateur

---
+++ site/TPI-11mai/TPI-2022/shoppers/coaches.php
@@ -0,0 +1,73 @@
+<?php
+
+///CPNV
+///Author : Mathias Groux
+///Date :10.05.2022
+///Description : This page displays the list of users
+
+// Include Database
+include_once ("DBAccess.php");
+include_once ("head.php");
+include_once ("header.php");
+?>
+    <html>
+    <div class="site-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="h3 mb-3 text-black">Liste des entraineurs</h2>
+                </div>
+                <div class="col-md-7">
+                    <div class="p-3 p-lg-5 border">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <label for="email" class="text-black">Liste des entraineurs</label>
+                                <br>
+                                <table style="width:100%">
+                                    <tr>
+                                        <th>Nom</th>
+                                        <th>___________</th>
+                                        <th>Prénom</th>
+                                        <th>id</th>
+                                        <th>Actions</th>
+                                    </tr>
+
+                                    <?php
+                                    //Call class
+                                    $db = new DBAccess();
+
+                                    //Research all fields information
+                                    $allCoaches = $db->getAllCoaches();
+
+                                    //This line would be great to be added
+                                    // <td><a href="#" class="btn btn-primary btn-sm">X</a></td>
+
+                                    //Display all fields values
+                                    foreach ($allCoaches as $coach)
+
+                                        echo"
+                                    <tr>
+                                        <td>$coach[firstname]</td>
+                                        <td>___________ </td>
+                                        <td>$coach[surname]</td>
+                                        <td>$coach[id]</td>  
+                                        <th>
+                                        <a href=\"detailUser.php ?id=$coach[id]\">Détail</a>
+</th>
+                                        <td></td>       
+                                    </tr>                                
+                                    ";
+                                    ?>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </html>
+<?php
+// Includes header and menu
+include_once ("footer-2.php");
+?>
---
+++ site/TPI-11mai/TPI-2022/shoppers/footer-1.php
@@ -0,0 +1,38 @@
+<!--
+
+//*********************************************************
+// Company  : CPNV
+// Author   : Mathias Groux
+// Date     : 04.05.2022
+// Summary  : This page in HTML is the larger footer.
+//*********************************************************
+
+
+-->
+
+<footer class="site-footer border-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="block-5 mb-5">
+                    <h3 class="footer-heading mb-4">information de contact</h3>
+                    <ul class="list-unstyled">
+                        <li class="address">Adresse à ne pas garder</li>
+                        <li class="phone"><a href="tel://23923929210">A ne pas garder</a></li>
+                        <li class="email">grouxmathias@cpnv.ch</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <p>
+
+                    adresseEmail@définir <i class="icon-heart" aria-hidden="true">ce truc est en italique</i>
+
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
